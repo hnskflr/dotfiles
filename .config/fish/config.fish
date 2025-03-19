9818383c4110f866e7bb18007623a919a650aae8
hnskflr <hkofler@protonmail.com>
2025-03-19 14:00:32 +0100

    bo

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -24,6 +24,15 @@ function f
     cd (cat $XDG_CACHE_HOME/fff/.fff_d)
 end
 
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
 zoxide init fish | source
 
 set -gx EDITOR nvim
