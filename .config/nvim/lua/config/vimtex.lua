a9f6d21950e9cbf4acb47640ee791f9bedbae303
hnskflr <hkofler@protonmail.com>
2025-08-27 10:48:21 +0200

    vim

---
+++ .config/nvim/lua/config/vimtex.lua
@@ -0,0 +1,5 @@
+vim.g.vimtex_view_method = "zathura"
+vim.g.maplocalleader = " "
+
+vim.g.vimtex_quickfix_enabled = 1
+vim.g.vimtex_quickfix_autoclose_after_keystrokes = 1
---
+++ .config/nvim/lua/config/vimwiki.lua
@@ -0,0 +1,7 @@
+vim.g.vimwiki_list = {{
+	path = '~/notes',
+	syntax = 'markdown',
+	ext = 'md'
+}}
+
+vim.g.vimwiki_global_ext = 0
